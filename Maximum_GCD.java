ceeb72f954a1992853e002d43943921f49bc9c15
utkarshsingh2122 <107643326+utkarshsingh2122@users.noreply.github.com>
2022-11-10 21:37:47 +0530

    Add files via upload

---
+++ Maximum_GCD.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Maximum_GCD {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n--){
+            int a = sc.nextInt();
+            System.out.println(a/2);
+        }
+    }
+}
